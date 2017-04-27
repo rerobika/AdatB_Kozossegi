ce5dc54948d3f2d0bd83ae600243b37a28b0216a
rozsapeter96 <rozsa.peter96@gmail.com>
2017-04-27 18:48:51 +0200

    Update KozossegiAlbum.java

--- src/kozossegi/view/elements/KozossegiAlbum.java
+++ src/kozossegi/view/elements/KozossegiAlbum.java
@@ -24,57 +24,26 @@ import kozossegi.view.elements.maincontent.KozossegiProfile;
 public class KozossegiAlbum extends JPanel {
 	private static final long serialVersionUID = 4821206698128240413L;
 	KozossegiAlbumBean data;
-	public KozossegiAlbum() {
-		KozossegiMainFrame mainFrame = KozossegiMainFrame.getInstance();	
-		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		JPanel head = new JPanel();
-		JPanel images = new JPanel();
-		JTextField text = new JTextField(20);
-		JLabel add = new JLabel(new ImageIcon(KozossegiImageManager.download(Labels.FILESERVER_PATH+"add_picture.png").getScaledInstance(64, 64, Image.SCALE_FAST)));
-		add.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent e) {
-				if(!text.getText().equals(""))
-				{
-					KozossegiPictureSelector pictureSelector = new KozossegiPictureSelector();
-					if(pictureSelector.isValidImage().equals(fileScan.SUCCES)){
-						mainFrame.getController().uploadPicture(pictureSelector.getSelectedFile(), text.getText(), mainFrame.getProfile().getId());
-					}
-				}
-			}
-		});
-		head.setLayout(new FlowLayout(FlowLayout.LEFT));
-		head.add(new JLabel(Labels.PROFIL_ADD_NEW));
-		head.add(text);
-		head.add(add);
-		head.setBorder(BorderFactory.createLineBorder(Color.black));
-		images.setLayout(new GridLayout(0, 5,2,2));		
-		images.add(add);
-		add.setBorder(BorderFactory.createLineBorder(Color.black));
-		add(head);
-		add(images);
-		setMaximumSize(getPreferredSize());
-	}
-	public KozossegiAlbum(KozossegiAlbumBean data,boolean editable)
-	{
-		
+
+	public KozossegiAlbum(KozossegiAlbumView albumview,KozossegiAlbumBean data) {
+
 		KozossegiMainFrame mainFrame = KozossegiMainFrame.getInstance();
-		this.data=data;
+		this.data = data;
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		JPanel head = new JPanel();
 		JPanel images = new JPanel();
 		head.setLayout(new FlowLayout(FlowLayout.LEFT));
 		head.add(new JLabel(data.getName()));
-		//head.setPreferredSize(new Dimension(mainFrame.getMainContentPanel().getWidth()-15, 40));
+		// head.setPreferredSize(new
+		// Dimension(mainFrame.getMainContentPanel().getWidth()-15, 40));
 		head.setBorder(BorderFactory.createLineBorder(Color.black));
-		images.setLayout(new GridLayout(0, 5,2,2));	
-		for(Image i : data.getImages())
-		{
+		images.setLayout(new GridLayout(0, 5, 2, 2));
+		for (Image i : data.getImages()) {
 			JLabel ilabel = new JLabel(new ImageIcon(i.getScaledInstance(64, 64, Image.SCALE_FAST)));
 			images.add(ilabel);
 			ilabel.setBorder(BorderFactory.createLineBorder(Color.black));
 		}
-		if(editable)
-		{
+		if (albumview.getProfile().getId()==mainFrame.getProfile().getId()) {
 			JLabel add = new JLabel(new ImageIcon(KozossegiImageManager
 					.download(Labels.FILESERVER_PATH + "add_picture.png").getScaledInstance(64, 64, Image.SCALE_FAST)));
 			add.addMouseListener(new MouseAdapter() {
